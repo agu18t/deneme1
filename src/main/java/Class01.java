613b1721c7900af548165d8e2c9d16531f93af38
Abdulkadir Bayram <bayram.abdulkadir111@gmail.com>
2023-05-18 22:18:58 +0300

    Version1

---
+++ src/main/java/Class01.java
@@ -0,0 +1,9 @@
+public class Class01 {
+    public static void main(String[] args) {
+        int x =3;
+        int y =5;
+        System.out.println(x+y);
+        System.out.println("versiyon1 için işlemler tamam");
+
+    }
+}
